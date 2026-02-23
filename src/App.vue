d430e4b9d72276ef61af5f282c3292b426f226d9
prestigeDevelop <67553154+prestigeDevelop@users.noreply.github.com>
2026-02-23 15:56:33 -0400

    update cart

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,81 @@
 <script setup>
-import { ref } from 'vue'
-import socksGreenImage from './assets/images/socks_green.jpeg'
-import socksBlueImage from './assets/images/socks_blue.jpeg'
+import { computed, ref } from "vue";
+import socksGreenImage from "./assets/images/socks_green.jpeg";
+import socksBlueImage from "./assets/images/socks_blue.jpeg";
 
-const product = ref('Socks')
-const brand = ref('Vue Mastery')
+const product = ref("Socks");
+const brand = ref("Vue Mastery");
 
-const image = ref(socksGreenImage)
-const inStock = ref(false)
-  
-const details = ref(['50% cotton', '30% wool', '20% polyester'])
+const selectedVariant = ref(0);
+
+const details = ref(["50% cotton", "30% wool", "20% polyester"]);
 
 const variants = ref([
-  { id: 2234, color: 'green', image: socksGreenImage },
-  { id: 2235, color: 'blue', image: socksBlueImage },
-])
+  {
+    id: 2234,
+    color: "green",
+    image: socksGreenImage,
+    quantity: 5,
+    price: 14,
+  },
+  { id: 2235, color: "blue", image: socksBlueImage, quantity: 5, price: 12.5 },
+]);
+const image = computed(() => {
+  return variants.value[selectedVariant.value].image;
+});
+const inStock = computed(() => {
+  return variants.value[selectedVariant.value].quantity > 0;
+});
+const price = computed(() => {
+  return "$" + variants.value[selectedVariant.value].price;
+});
+const title = computed(() => {
+  return brand.value + " " + product.value;
+});
+const cart = ref([]);
 
-const cart = ref(0)
+const addToCart = () => {
+  cart.value.push(variants.value[selectedVariant.value]);
+  variants.value[selectedVariant.value].quantity--;
+  total.value += variants.value[selectedVariant.value].price;
+};
 
-const addToCart = () => cart.value += 1
+const updateQuantity = (index) => {
+  selectedVariant.value = index;
+  console.log(cart.value);
+};
 
-const updateImage = (variantImage) => image.value = variantImage
-
+const total = ref(0);
 </script>
-  
+
 <template>
   <div class="nav-bar"></div>
-  <div class="cart">Cart({{ cart }})</div>
+  <div class="cart">Cart({{ cart.length }})</div>
+  <div class="cart">Cart Total(${{ total }})</div>
   <div class="product-display">
     <div class="product-container">
-      <div class="product-image">    
-        <img v-bind:src="image">
+      <div class="product-image">
+        <img v-bind:src="image" :class="{ 'out-of-stock-img': !inStock }" />
       </div>
       <div class="product-info">
-        <h1>{{ product }}</h1>
-        <p v-if="inStock">In Stock</p>
+        <h1>{{ title }}</h1>
+        <p v-if="inStock">In Stock:{{ price }}</p>
         <p v-else>Out of Stock</p>
         <ul>
           <li v-for="detail in details">{{ detail }}</li>
         </ul>
-        <div 
-          v-for="variant in variants" 
+        <div
+          v-for="(variant, index) in variants"
           :key="variant.id"
-          @mouseover="updateImage(variant.image)"
+          @mouseover="updateQuantity(index)"
           class="color-circle"
           :style="{ backgroundColor: variant.color }"
-        >
-        </div>
+        ></div>
         <button
-          class="button" 
+          class="button"
           :class="{ disabledButton: !inStock }"
           :disabled="!inStock"
-          v-on:click="addToCart"
+          v-on:click="addToCart()"
         >
           Add to cart
         </button>
