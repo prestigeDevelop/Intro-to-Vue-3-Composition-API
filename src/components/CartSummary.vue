5669468707ac29d045d246fdb514ac8652362746
prestigeDevelop <67553154+prestigeDevelop@users.noreply.github.com>
2026-02-28 14:54:27 -0400

    update cartSummary

--- src/components/CartSummary.vue
+++ src/components/CartSummary.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { inject, computed } from "vue";
+import { inject, computed, ref } from "vue";
 
 const cart = inject("cart");
 const total = inject("total");
@@ -8,6 +8,35 @@ const user = inject("user");
 const getQuantityByProduct = (productId) => {
   return cart.value.filter((item) => item.id === productId).length;
 };
+
+const getReducedCart = computed(() => {
+  return cart.value.reduce((acc, item) => {
+    const existingItem = acc.find((i) => i.id === item.id);
+    console.log(existingItem);
+
+    if (existingItem) {
+      existingItem.quantity += 1;
+      existingItem.total = existingItem.quantity * existingItem.price;
+    } else {
+      acc.push({ ...item, quantity: 1, total: item.price });
+    }
+    console.log(acc);
+    return acc;
+  }, []);
+});
+
+const decrement = (item) => {
+  if (cart.value.filter((i) => i.id === item.id).length > 0) {
+    const index = cart.value.findIndex((i) => i.id === item.id);
+    cart.value.splice(index, 1);
+    total.value -= item.price;
+  }
+};
+
+const increment = (item) => {
+  cart.value.push(item);
+  total.value += item.price;
+};
 </script>
 
 <template>
@@ -15,16 +44,22 @@ const getQuantityByProduct = (productId) => {
     <div class="product-display">
       <h2 class="mb-2">Cart Summary for:{{ user }}</h2>
       <div class="row mb-3">
-        <div class="col-4 cartTable">Product Details</div>
-        <div class="col-4 cartTable">Quantity</div>
-        <div class="col-4 cartTable">Price</div>
+        <div class="col-3 cartTable">Product Details</div>
+        <div class="col-3 cartTable">Quantity</div>
+        <div class="col-3 cartTable">Price</div>
+        <div class="col-3 cartTable">Total</div>
       </div>
-      <div class="row" v-for="(item, index) in cart" :key="index">
-        <div class="col-4 d-flex align-items-center gap-2">
+      <div class="row" v-for="(item, index) in getReducedCart" :key="index">
+        <div class="col-3 d-flex align-items-center gap-2">
           <img v-bind:src="item.image" class="col-2 m-0 p-0" />{{ item.name }}
         </div>
-        <div class="col-4">{{ getQuantityByProduct(item.id) }}</div>
-        <div class="col-4">${{ item.price }}</div>
+        <div class="col-3">
+          <button class="buttonPlusMinus" @click="decrement(item)">-</button
+          >{{ item.quantity
+          }}<button class="buttonPlusMinus" @click="increment(item)">+</button>
+        </div>
+        <div class="col-3">${{ item.price }}</div>
+        <div class="col-3">{{ item.total }}</div>
       </div>
       <p class="mt-2">
         <strong>Total: ${{ total }}</strong>
